cb8840afce24128fea8ba373196b4f21ace3ab77
zhiidetopchubaeva <ms.jiyd@gmail.com>
2022-08-22 16:25:34 +0600

    changes

--- toys/admin.py
+++ toys/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Category, Toy, Like, Rating, Comment
+from .models import Category, Toy, Like, Rating, Comment, Favorite
 
 admin.site.register(Category)
 admin.site.register(Toy)
 admin.site.register(Like)
 admin.site.register(Rating)
 admin.site.register(Comment)
+admin.site.register(Favorite)
