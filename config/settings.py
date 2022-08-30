ac3e1ab74ff95ef45f504e67c4624c02870ec3f1
Zonder9162 <zonder9162@gmail.com>
2022-08-30 17:35:33 +0600

    564738198363

--- config/settings.py
+++ config/settings.py
@@ -110,26 +110,26 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
-}
-
-# import dj_database_url
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql'
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': config('DB_NAME'),
+#         'USER': config('DB_USER'),
+#         'PASSWORD': config('DB_PASSWORD'),
+#         'HOST': 'localhost',
+#         'PORT': '5432'
 #     }
-# } 
+# }
 
-# db = dj_database_url.config(conn_max_age=600)
-# DATABASES['default'].update(db)
+import dj_database_url
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql'
+    }
+} 
+
+db = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db)
 
 # if DEBUG:
 #     DATABASES = {
